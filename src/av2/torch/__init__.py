fd0884af96b917cf3f5aa074a052c348bd4cd7de
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2022-10-16 04:32:21 -0700

    Remove force multiprocessing.

--- src/av2/torch/__init__.py
+++ src/av2/torch/__init__.py
@@ -7,9 +7,9 @@ logger = logging.getLogger(__name__)
 try:
     import torch.multiprocessing as mp
 
+    mp.set_forkserver_preload(["polars"])
     if mp.get_start_method() != "forkserver":
         logging.warning("Setting multiprocessing start method to forkserver to avoid deadlocking.")
-        mp.set_start_method("forkserver", force=True)
-    mp.set_forkserver_preload(["polars"])
+        mp.set_start_method("forkserver")
 except ImportError as _:
     logger.error("Please install Pytorch to use this module.")
