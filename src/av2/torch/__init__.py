4d40f49b542158e720aa8845a6e7f8eede077801
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2022-10-16 08:18:06 -0700

    Update __init__.py

--- src/av2/torch/__init__.py
+++ src/av2/torch/__init__.py
@@ -5,11 +5,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 try:
-    import torch.multiprocessing as mp
-
-    mp.set_forkserver_preload(["polars"])
-    if mp.get_start_method() != "forkserver":
-        logging.warning("Setting multiprocessing start method to forkserver to avoid deadlocking.")
-        mp.set_start_method("forkserver")
+    import torch
 except ImportError as _:
     logger.error("Please install Pytorch to use this module.")
